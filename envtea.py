bab7a2321a1e3f2603b9e93c1204390a3d4831e9
komaru.yml <winbo.yml@proton.me>
2025-04-10 15:30:15 +0200

    envtea: Add a script for starting and placeholder Python file

---
+++ envtea.py
@@ -0,0 +1,7 @@
+# This file is part of Envtea, a tool written in Python for managing your
+# system and quickly switching between environments in seconds.
+#
+# Read the LICENSE file for details about licensing.
+# If this file isn't included in your source code, you may have a redistribution by someone trying to hack you!
+
+# Placeholder
